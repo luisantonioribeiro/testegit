fe51dff4886dfd46270583bdd174dc95b0e2a184
Luis <nucleodeproducoes@hotmail.com>
2021-07-06 21:36:47 -0300

    Projeto de Teste

---
+++ Primeiro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Concurrent;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Course
+{
+    class Program
+    {
+        List<string> list = new List<string>();
+
+        List.
+        
+
+    }
+}
